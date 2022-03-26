5fcec41e94b11fd069047e3ba79b81564f1fe257
markus.dymalla@canstar.com.au <markus.dymalla@canstar.com.au>
2022-03-26 10:43:40 +1000

    initial commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+
+	default:
+		panic("bad command")
+	}
+}
+
+func run() {
+	// cmd struct to execute named program with given arguments
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// operating system specific attributes - run passes it to new process as sys fields
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v as process %d inside container\n", os.Args[2:], os.Getpid())
+	fmt.Printf("-------------------------------------------------------------\n")
+
+	syscall.Sethostname([]byte(os.Args[3]))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo go run main.go run $1
